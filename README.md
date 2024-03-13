317cc4cd384ea8b81f71f13c0c11675c3034421d
Brandon Fowler <brandfowl@gmail.com>
2024-03-13 02:31:16 -0400

    Grammar

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Genshin Artifact EXP Calculator
 
-Calculate the level artifacts and artifact enhancement materials will being an artifact too and how much artifact EXP / resin is needed to reach a specific level.
+Calculate the level fodder artifacts and artifact enhancement materials will bring an artifact to and how much artifact EXP / resin is needed to reach a specific level.
--- src/lib/components/title.svelte
+++ src/lib/components/title.svelte
@@ -1,7 +1,7 @@
 <header>
 	<h1 class="mb-4 text-3xl font-bold">Genshin Artifact EXP Calculator</h1>
 	<p>
-		Calculate the level artifacts and artifact enhancement materials will being an artifact too and
-		how much artifact EXP / resin is needed to reach a specific level.
+		Calculate the level fodder artifacts and artifact enhancement materials will bring an artifact
+		to and how much artifact EXP / resin is needed to reach a specific level.
 	</p>
 </header>
